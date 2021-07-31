edaa7b41f2a5b55c579d97799840fd47929ed9c9
Rafael <rafael.2018.ads@gmail.com>
2021-07-31 18:23:28 -0300

    Criado a base do projeto

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const app = express();
 
+const connection = require('./src/database/database');
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/src/views');
+
+connection.authenticate()
+    .then(() => {
+        console.log('Autenticado com sucesso!');
+    }).catch((error) => {
+        console.log(error);
+    });
+
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-    res.send('Funcionando.');
+    res.render('index');
 });
 
 app.listen(8080, () => {
---
+++ src/database/database.js
@@ -0,0 +1,9 @@
+const Sequelize = require('sequelize');
+
+const connection = new Sequelize('guiapress', 'root', '',
+    {
+        host: 'localhost',
+        dialect: 'mysql'
+    });
+
+module.exports = connection;
