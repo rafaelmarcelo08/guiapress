7d262aea467d502a2bcf8828b097f4aedf3c1113
Rafael <rafael.2018.ads@gmail.com>
2021-08-01 10:06:25 -0300

    Criado o formulario de categoria

--- src/categories/Controllers/CategoryController.js
+++ src/categories/Controllers/CategoryController.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/categories', (req, res) => {
-    res.send('rota de categorias')
+router.get('/admin/categories/new', (req, res) => {
+    res.render('admin/categories/new');
 });
 
 module.exports = router;
