7b6f62794f1df9f2c831af5bd22b4832c4a9d75e
Rafael <rafael.2018.ads@gmail.com>
2021-08-01 18:28:08 -0300

    Criado e testado a insercao de categorias no banco de dados

--- src/categories/Controllers/CategoryController.js
+++ src/categories/Controllers/CategoryController.js
@@ -1,8 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const slugify = require('slugify');
+
+const Category = require('../model/Category');
 
 router.get('/admin/categories/new', (req, res) => {
     res.render('admin/categories/new');
+});
+
+router.post('/categories/save', (req, res) => {
+    let { title } = req.body;
+
+    if (title != undefined) {
+        Category.create(
+            {
+                title: title,
+                slug: slugify(title)
+            }
+        ).then(() => {
+            console.log('Categoria salva.');
+            //res.redirect('/admin/categories/new');
+        }).catch((error) => {
+            console.log(error);
+        });
+    } else {
+        res.redirect('admin/categories/new');
+    }
 });
 
 module.exports = router;
