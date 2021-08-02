ea84c0c4099e2f29a0c69c3c7beea9b2b548af8d
Rafael <rafael.2018.ads@gmail.com>
2021-08-02 18:39:57 -0300

    Criado o formulario de adicao de artigo

--- src/articles/Controllers/ArticleController.js
+++ src/articles/Controllers/ArticleController.js
@@ -5,4 +5,8 @@ router.get('/article', (req, res) => {
     res.send('rota de articles')
 });
 
+router.get('/admin/articles/new', (req, res) => {
+    res.render('admin/articles/new')
+});
+
 module.exports = router;
