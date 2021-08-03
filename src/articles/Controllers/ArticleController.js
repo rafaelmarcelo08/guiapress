d5c01fbd2335027f30675219fbd170ebd6f680c7
Rafael <rafael.2018.ads@gmail.com>
2021-08-03 10:05:11 -0300

    Criado a listagem de categorias no formulario de artigos

--- src/articles/Controllers/ArticleController.js
+++ src/articles/Controllers/ArticleController.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const router = express.Router();
 
+const Category = require('../../categories/model/Category');
+
 router.get('/article', (req, res) => {
-    res.send('rota de articles')
+    res.send('rota de articles');
 });
 
 router.get('/admin/articles/new', (req, res) => {
-    res.render('admin/articles/new')
+    Category.findAll(
+        {
+            raw: true
+        }
+    ).then((categories) => {
+
+        res.render('admin/articles/new',
+            {
+                categories: categories
+            }
+        );
+    });
 });
 
 module.exports = router;
