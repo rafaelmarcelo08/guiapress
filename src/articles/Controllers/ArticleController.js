c487948072a29905820d7fa5c80a3d16d7a92e34
Rafael <rafael.2018.ads@gmail.com>
2021-08-03 10:24:07 -0300

    Ajustado o tempo de criacao de um objeto no sequelize

--- src/articles/Controllers/ArticleController.js
+++ src/articles/Controllers/ArticleController.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const slugify = require('slugify');
 
 const Category = require('../../categories/model/Category');
+const Article = require('../model/Article');
 
-router.get('/article', (req, res) => {
+router.get('/admin/article', (req, res) => {
     res.send('rota de articles');
 });
 
@@ -19,6 +21,24 @@ router.get('/admin/articles/new', (req, res) => {
                 categories: categories
             }
         );
+    });
+});
+
+router.post('/articles/save', (req, res) => {
+    let { category } = req.body;
+    let { title } = req.body;
+    let { body } = req.body;
+
+    Article.create(
+        {
+            title: title,
+            slug: slugify(title),
+            body: body,
+            categoryId: category
+        }
+    ).then(() => {
+        console.log('Artigo salvo com sucesso.');
+        res.redirect('/admin/article');
     });
 });
 
--- src/database/database.js
+++ src/database/database.js
@@ -3,7 +3,8 @@ const Sequelize = require('sequelize');
 const connection = new Sequelize('guiapress', 'root', '',
     {
         host: 'localhost',
-        dialect: 'mysql'
+        dialect: 'mysql',
+        timezone: '-03:00'
     });
 
 module.exports = connection;
